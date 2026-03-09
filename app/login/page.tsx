b444550f158e16f59deeae3a9a5f1707c26638f7
joySarkar <joysarkar98600@gmail.com>
2026-03-10 01:53:52 +0600

    complete login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,13 @@
+import { LoginForm } from "@/components/modules/auth/login/LoginForm";
+
 const page = () => {
-    return <div>login</div>
-}
+  return (
+    <div className="container mx-auto px-4">
+      <div className="min-h-[80vh] flex items-center justify-center">
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
 
 export default page;
